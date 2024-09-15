e462b486a100b787d0edae8632fd11d1817b5429
Vijayakanth M <vikymahendiran123@gmail.com>
2024-09-15 18:04:16 +0530

    cors2

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { exec } = require('child_process');
+const cors= require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors());
 
 let requestCount = {};
 const MAX_REQUESTS = 5;
