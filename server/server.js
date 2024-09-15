a252a11add49a97a73b41ce8c0ca8c640423c3d6
Vijayakanth M <vikymahendiran123@gmail.com>
2024-09-15 17:41:13 +0530

    error checking

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
 app.use(express.static('public'));
 
